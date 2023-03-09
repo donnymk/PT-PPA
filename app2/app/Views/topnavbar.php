9c4bad721137867322dcb0542ead2ed1636872b1
Donny Malik Kurniawan <bossdony@gmail.com>
2023-03-09 16:29:54 +0700

    Update topnavbar.php

--- app2/app/Views/topnavbar.php
+++ app2/app/Views/topnavbar.php
@@ -1,5 +1,5 @@
 <!-- Navbar Brand-->
-<a class="navbar-brand ps-3" href="index.html">Claim Warranty</a>
+<a class="navbar-brand ps-3" href="<?= base_url('claim-warranty') ?>">Claim Warranty</a>
 <!-- Sidebar Toggle-->
 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
 <!-- Navbar Search-->
