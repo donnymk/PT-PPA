0ef21cb5a7677c42953b286e3ca3c611f6864a94
Donny Malik Kurniawan <bossdony@gmail.com>
2023-08-08 14:55:13 +0700

    Update dasborcwp.php

--- app2/app/Views/dasborcwp.php
+++ app2/app/Views/dasborcwp.php
@@ -116,9 +116,6 @@
             plugins: {
                 legend: {
                     display: false
-                },
-                colors: {
-                    enabled: true
                 }
             }
         };
@@ -132,6 +129,20 @@
                         label: 'Jumlah CWP',
                         //backgroundColor: 'rgb(252, 116, 101)',
                         data: value,
+						backgroundColor: [
+						  'rgba(255, 99, 132, 0.2)',
+						  'rgba(255, 159, 64, 0.2)',
+						  'rgba(75, 192, 192, 0.2)',
+						  'rgba(54, 162, 235, 0.2)',
+						  'rgba(153, 102, 255, 0.2)'
+						],
+						borderColor: [
+						  'rgb(255, 99, 132)',
+						  'rgb(255, 159, 64)',
+						  'rgb(75, 192, 192)',
+						  'rgb(54, 162, 235)',
+						  'rgb(153, 102, 255)'
+						],
                         borderWidth: 2
                     }]
             },
